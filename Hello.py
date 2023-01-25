d32b2d7e7a9b573341f4c0c7b3a0788571eca01f
aguk200 <109250490+aguk200@users.noreply.github.com>
2023-01-26 09:49:56 +1000

    Updated the python script

--- Hello.py
+++ Hello.py
@@ -1,5 +1,18 @@
 from datetime import datetime
 
-a = datetime.now()
+FN = "test.txt"
 
+i = 2
+fa = open(FN,'a')
+fr = open(FN,'r')
+b = len(fr.readlines())
+
+a = datetime.now()
 print ("Hello", a)
+
+if (i==2):
+    print ("Line ",b,": ",a, file=fa)
+
+print (b)
+fa.close()
+fr.close()
